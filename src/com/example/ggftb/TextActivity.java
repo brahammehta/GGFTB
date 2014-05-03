eaa705bff79bff0890f9436d674a31876bcaf4a5
Max <maxwellnoah@gmail.com>
2014-05-03 12:14:30 -0700

    Test Commit /text

--- src/com/example/ggftb/TextActivity.java
+++ src/com/example/ggftb/TextActivity.java
@@ -15,6 +15,7 @@ public class TextActivity extends Activity {
 		@Override
 		public boolean onCreateOptionsMenu(Menu menu) {
 			// Inflate the menu; this adds items to the action bar if it is present.
+            //Test test test 
 			getMenuInflater().inflate(R.menu.main, menu);
 			return true;
 		}
