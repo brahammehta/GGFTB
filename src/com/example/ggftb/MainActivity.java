fbd00661856358ac843398dcb2bd4473edddbc65
brahammehta <brahammehta@gmail.com>
2014-05-03 11:42:34 -0700

    Original Layout
    All Activities Set Up but Blank
    3 Buttons to Launch Activities (Crowdsource, Text to Speech, and Picture
    to Speech)
    May have bugs in manifest

---
+++ src/com/example/ggftb/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.ggftb;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	public void sendCrowdsource(View view) {
+	    //Do something in response to button
+		Intent intent = new Intent(this, Crowdsource.class);
+		startActivity(intent);
+	}
+	public void sendPicture(View view) {
+	    //Do something in response to button
+		Intent intent = new Intent(this, Picture.class);
+		startActivity(intent);
+	}
+	public void sendText(View view) {
+	    //Do something in response to button
+		Intent intent = new Intent(this, Text.class);
+		startActivity(intent);
+	}
+}
